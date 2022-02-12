df67972eee3c5837014e35dba574e499cad3e47b
crystal <crystallwi.xiaujing@petronas.com>
2022-02-12 12:05:42 +0800

    [requirements.R]first commit requirements.

---
+++ sm-r/requirements.R
@@ -0,0 +1,6 @@
+##load libraries ####
+install.packages(c('pacman','forecastML', 'recipes', 'timetk', 'rsample', 'workflows', 'rlang', 'stringr',
+                    'parsnip', 'modeltime', 'glmnet', 'mltools', 'doSNOW', 'glue', 'lubridate', 'purrr','data.table', "here",
+                    "RPostgreSQL", "DBI", "remotes", "reticulate", "caret", "futile.logger", "formatR", 'tidyr',
+                    "aws.s3", "astsa", "reshape2", "tryCatchLog", "tidyverse", "dplyr", "randomForest", "kernlab",
+                    "janitor", "readxl", "forecast", "doParallel", "reshape2", "tseries", "zoo", 'paws', 'config'))
