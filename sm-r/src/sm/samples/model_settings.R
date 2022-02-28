e35815c1e698dc9af8f2aba73b81375fb1af38f9
crystal <crystallwi.xiaujing@petronas.com>
2022-02-28 23:10:16 +0800

    Updated to read from docker local.

--- sm-r/src/sm/samples/model_settings.R
+++ sm-r/src/sm/samples/model_settings.R
@@ -4,10 +4,11 @@ control <- trainControl(method="cv", number=10)
 metric <- "Accuracy"
 
 # Load data from Github Example. Requires internet.
-data <- read.csv("https://raw.githubusercontent.com/trevorwitter/Iris-classification-R/master/iris.csv")
-#load data from CSV file
-# data <- read.csv("iris.csv", header=FALSE) 
+# data <- read.csv("https://raw.githubusercontent.com/trevorwitter/Iris-classification-R/master/iris.csv")
 
+#load data from CSV file
+data <- read.csv(paste0(here::here(), "/src/sm/samples/iris.csv"), header=FALSE) 
+# write.csv(data, paste0(here::here(), "/src/sm/samples/iris.csv"))
 # et data frame column names 
 colnames(data) <- c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species")
 
