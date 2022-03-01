ff4bbb0bdd80725379f68b1626958822174d3a30
crystal <crystallwi.xiaujing@petronas.com>
2022-03-01 10:34:25 +0800

    Updated gitignore. Updated listfiles setup sysenv.

--- sm-r/src/sm/samples/model_settings.R
+++ sm-r/src/sm/samples/model_settings.R
@@ -7,7 +7,9 @@ metric <- "Accuracy"
 # data <- read.csv("https://raw.githubusercontent.com/trevorwitter/Iris-classification-R/master/iris.csv")
 
 #load data from CSV file
-data <- read.csv(paste0(here::here(), "/src/sm/samples/iris.csv"), header=FALSE) 
+data <- read.csv(paste0(here::here(), "/src/sm/samples/iris.csv"), header=FALSE) %>% 
+  dplyr::select(-c(V1)) %>% 
+  dplyr::slice(n= 2:nrow(.))# remove first column
 # write.csv(data, paste0(here::here(), "/src/sm/samples/iris.csv"))
 # et data frame column names 
 colnames(data) <- c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species")
